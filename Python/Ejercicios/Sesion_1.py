f3bafd2f7b3e42f7e69380777283734580226362
Xhinen <xhinen@proton.me>
2025-03-10 19:07:54 +0100

    actualización repositorio

---
+++ Python/Ejercicios/Sesion_1.py
@@ -0,0 +1,16 @@
+# EJERCICIO PRÁCTICO 3️⃣:
+# Crea una variable con tu nombre y apellidos y muestra su longitud.
+nombre = "Alberto Mira"
+print("La longitud de mi nombre es: ", len(nombre))
+
+# Concatena tu nombre con una frase de presentación.
+print("Hola a todos mi llamo " + nombre)
+
+# Convierte tu nombre en mayúsculas y minúsculas.
+mayuscula = nombre.upper()
+minuscula = nombre.lower()
+print("Mi nombre en minúsculas: ", minuscula)
+print("Mi nombre en mayúsculas: ", mayuscula)
+
+# Extrae los primeros 3 caracteres de tu nombre usando slicing.
+print("Los 3 primeros caracteres de mi nombre son: ", nombre[0:3])
