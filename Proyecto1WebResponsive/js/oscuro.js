694216d4f9cd694e44dbd5f6bbfed412d4af67c0
Xhinen <xhinen@proton.me>
2025-07-06 10:29:19 +0200

    pequeñas mejoras en el menu responsive cuando la pantalla es inferior a 770px

--- Proyecto1WebResponsive/js/oscuro.js
+++ Proyecto1WebResponsive/js/oscuro.js
@@ -1,8 +1,22 @@
 /* TOGGLE */
-const boton = document.getElementById("toggleTema");
+const boton = document.getElementById("modoOscuro");
+
+// Comprobamos al cargar la web si el modo oscuro se activó con anterioridad
+if (localStorage.getItem("tema") === "oscuro"){
+    document.body.classList.add("oscuro");
+}
 
 boton.addEventListener("click",oscuro);
 
 function oscuro(){
     document.body.classList.toggle("oscuro");
+
+
+    // Guardar la preferencia en localStorage
+    if (document.body.classList.contains("oscuro")){
+        localStorage.setItem("tema", "oscuro");
+    } else {
+        localStorage.setItem("tema", "claro");
+    }
 }
+
